9e6bc086126446a7c3f9b91c99bf2dec72b46e6b
MinhQuan-Github <minhquan.5322@aol.com>
2022-01-21 23:32:02 +0700

    update

--- I Am Rich (swiftUI)/ContentView.swift
+++ I Am Rich (swiftUI)/ContentView.swift
@@ -9,13 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(.systemBlue)
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                Text("I Am Rich")
+                    .fontWeight(.bold)
+                    .padding()
+                    .font(.system(size: 40))
+                    .foregroundColor(Color.yellow)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 250, height: 250, alignment: .center)
+            }
+                
+        }
     }
 }   
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice(PreviewDevice(rawValue: "iphone 11"))
     }
 }
+	
