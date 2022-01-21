9d86726ee8771df55da172c2cb6a7a5c9a4b1d1b
MinhQuan-Github <minhquan.5322@aol.com>
2022-01-21 21:53:21 +0700

    create project

---
+++ I Am Rich (swiftUI)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I Am Rich (swiftUI)
+//
+//  Created by Quan's Macbook on 21/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}   
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ I Am Rich (swiftUI)/I_Am_Rich__swiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_Rich__swiftUI_App.swift
+//  I Am Rich (swiftUI)
+//
+//  Created by Quan's Macbook on 21/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_Rich__swiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
